f341cc6f635ae939d45d8801fab0346eb861c5fd
danilloboing <danillo.boing.souza@gmail.com>
2023-01-30 16:07:28 -0300

    updated forms and fixed the formula

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 var result = 0;
 var months = 0;
-const resultado = months
 var botaoCalcular = document.querySelector("#calcular");
 botaoCalcular.addEventListener("click", function(event) {
   event.preventDefault();
 
-    var form = document.querySelector("#form-calculo");
-    var aporteMensal = document.querySelector('#aporteMensal').value;
-    var jurosAnual = document.querySelector('#jurosAnual').value;
-  if (isNaN(aporteMensal) || aporteMensal <= 0) {
-            alert('Preço deve ser um número maior que zero');
-            return; 
-        }
-        if (isNaN(jurosAnual) || jurosAnual < 0) {
-            alert('O juros deve ser um número maior que zero');
-            return;
-        }
-        while (result < 1000000) {
-          var juros = (jurosAnual / 100) / 12
-          result = (result + aporteMensal) * (juros);
+    var aporteMensal = parseFloat(document.querySelector('#aporteMensal').value);
+    var jurosAnual = parseFloat(document.querySelector('#jurosAnual').value);
+    
+    if (isNaN(aporteMensal) || aporteMensal <= 0) {
+      alert('Preço deve ser um número maior que zero');
+      return; 
+    }
+    if (isNaN(jurosAnual) || jurosAnual < 0) {
+      alert('O juros deve ser um número maior que zero');
+      return;
+    }
+    while (result < 1000000) {
+          result = (result + aporteMensal) * (((jurosAnual / 100 ) / 12) + 1);
           months++;
         }
         console.log (months)
-        document.getElementById("resultado").innerHTML = "Considerando seu aporte mensal e juros anual, você alcançará seu primeiro milhão em " + months + " meses."
+        console.log (jurosAnual)
+        console.log (result)
+
+        document.getElementById("resultado").innerHTML = "Considerando seu aporte mensal e juros anual, você alcançará seu primeiro milhão em aproximadamente " + months + " meses."
+        document.getElementById("alerta-calculo").innerHTML = ""
+
       })
